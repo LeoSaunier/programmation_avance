b211ab16d1f0ad1ea11b00f27d7c11b41bb8e59e
Compte Temporaire INFO 3 <temporaire-info3@or-iut-i102-f22.iut45.univ-orleans.fr>
2025-09-04 09:48:38 +0200

    Ajout des pages contact et about

--- TutoDjango/monApp/urls.py
+++ TutoDjango/monApp/urls.py
@@ -3,4 +3,6 @@ from . import views
 
 urlpatterns = [
 path("", views.home, name="home"),
+path("aboutus", views.about_us, name="aboutus"),
+path("contactus", views.contact_us, name="contactus"),
 ]
