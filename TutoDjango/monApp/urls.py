c7cc05c78395ceb4bc9761f6b47ca0a2bf4b9b79
Compte Temporaire INFO 3 <temporaire-info3@or-iut-i102-f22.iut45.univ-orleans.fr>
2025-09-04 09:57:24 +0200

    Challenge aboutus et contactus fini

--- TutoDjango/monApp/urls.py
+++ TutoDjango/monApp/urls.py
@@ -2,7 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-path("", views.home, name="home"),
+path('home/<param>',views.home ,name='home'),
+path('home',views.home_sans_param ,name='home'),
 path("aboutus", views.about_us, name="aboutus"),
 path("contactus", views.contact_us, name="contactus"),
 ]
--- TutoDjango/monApp/views.py
+++ TutoDjango/monApp/views.py
@@ -1,8 +1,11 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
-def home(request):
-    return HttpResponse("<h1>Hello Django!</h1>")
+def home(request, param):
+    return HttpResponse("<h1>Bonjour "+param+" !!!</h1>")
+
+def home_sans_param(request):
+    return HttpResponse("<h1>Bonjour !!!</h1>")
 
 def about_us(request):
     return HttpResponse("<h1>About Us</h1>")
