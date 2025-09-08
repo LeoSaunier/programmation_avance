a65532311ce56f4554af1e7e54ae61093747aa99
LeoSaunier <leo.saunier@etu.univ-orleans.fr>
2025-09-08 16:23:55 +0200

    Listes Categories et Statuts

--- TutoDjango/monApp/urls.py
+++ TutoDjango/monApp/urls.py
@@ -7,4 +7,6 @@ path('home',views.home_sans_param ,name='home'),
 path("aboutus", views.about_us, name="aboutus"),
 path("contactus", views.contact_us, name="contactus"),
 path("produits", views.list_produits, name="produits"),
+path("categories", views.list_categories, name="categories"),
+path("statuts", views.list_statuts, name="statuts"),
 ]
--- TutoDjango/monApp/views.py
+++ TutoDjango/monApp/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-from .models import Produit
+from .models import Produit, Statut, Categorie
 
 def home(request, param):
     return HttpResponse("<h1>Bonjour "+param+" !!!</h1>")
@@ -28,6 +28,38 @@ def list_produits(request):
     """
 
     return HttpResponse(html)
+
+def list_statuts(request):
+    statuts = Statut.objects.all()
+    statut_html = ""
+    for s in statuts:
+        statut_html+=f"<li>{s.libelle}</li>\n"
+
+    html= f"""
+    <h1> Produits </h1>
+    <ul> 
+        {statut_html}
+    </ul>
+    """
+
+    return HttpResponse(html)
+
+
+def list_categories(request):
+    ctgrs = Categorie.objects.all()
+    categorie_html = ""
+    for ctgr in ctgrs:
+        categorie_html+=f"<li>{ctgr.nomCat}</li>\n"
+
+    html= f"""
+    <h1> Produits </h1>
+    <ul> 
+        {categorie_html}
+    </ul>
+    """
+
+    return HttpResponse(html)
+
 
 
 # Create your views here.
