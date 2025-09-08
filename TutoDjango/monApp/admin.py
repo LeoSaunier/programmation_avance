c9c54e4cc31b2d21d103ee813f1e58943d6eb78c
LeoSaunier <leo.saunier@etu.univ-orleans.fr>
2025-09-08 16:56:56 +0200

    Affichage des autre classes du models

--- TutoDjango/monApp/admin.py
+++ TutoDjango/monApp/admin.py
@@ -1,6 +1,24 @@
 from django.contrib import admin
-from .models import Produit
+from .models import Produit, Categorie, Statut, Rayon, Contenir
 
-admin.site.register(Produit)
+class ProduitAdmin(admin.ModelAdmin):
+    list_display = ('intituleProd','prixUnitaireProd')
+
+class ProduitInline(admin.TabularInline):
+    model = Produit
+    extra = 1 # nombre de lignes vides par défaut
+
+class CategorieAdmin(admin.ModelAdmin):
+    model = Categorie
+    inlines = [ProduitInline]
+
+admin.site.register(Produit, ProduitAdmin)
+admin.site.register(Categorie, CategorieAdmin)
+admin.site.register(Statut)
+admin.site.register(Rayon)
+admin.site.register(Contenir)
+
+
+
 
 # Register your models here.
