709f750a16b3bb550eb586618396b3292d154c09
LeoSaunier <leo.saunier@etu.univ-orleans.fr>
2025-09-11 10:04:47 +0200

    ajout filter online/offline

--- TutoDjango/monApp/admin.py
+++ TutoDjango/monApp/admin.py
@@ -1,10 +1,28 @@
 from django.contrib import admin
 from .models import Produit, Categorie, Statut, Rayon, Contenir
 
+
+class ProduitFilter(admin.SimpleListFilter):
+    title = 'filtre produit'
+    parameter_name = 'custom_statut'
+    def lookups(self, request, model_admin) :
+        return (
+        ('OnLine', 'En ligne'),
+        ('OffLine', 'Hors ligne'),
+        )
+    def queryset(self, request, queryset):
+        if self.value() == 'OnLine':
+            return queryset.filter(statut=1)
+        if self.value() == 'OffLine':
+            return queryset.filter(statut=0)
+
 class ProduitAdmin(admin.ModelAdmin):
     model = Produit
     list_display = ["refProd", "intituleProd", "prixUnitaireProd", "date_fabrication", "categorie", "statut"]
-    list_editable = ["intituleProd", "prixUnitaireProd", "date_fabrication"]
+    list_editable = ["intituleProd", "prixUnitaireProd", "date_fabrication", "statut"]
+    radio_fields = {"statut": admin.VERTICAL}
+    search_fields = ('intituleProd', 'date_fabrication')
+    list_filter = (ProduitFilter,)
 
 class ProduitInline(admin.TabularInline):
     model = Produit
