5d188415db615c30644545dc56e9a5f193060531
LeoSaunier <leo.saunier@etu.univ-orleans.fr>
2025-09-15 16:01:06 +0200

    About us et contact us en template

--- TutoDjango/monApp/views.py
+++ TutoDjango/monApp/views.py
@@ -11,10 +11,10 @@ def home_sans_param(request):
     return HttpResponse("Bonjour Monde!")
 
 def about_us(request):
-    return HttpResponse("<h1>About Us</h1>")
+    return render(request, 'monApp/about_us.html')
 
 def contact_us(request):
-    return HttpResponse("<h1>Contact Us</h1>")
+    return render(request, 'monApp/contact_us.html')
 
 def ListProduits(request):
     prdts = Produit.objects.all()
